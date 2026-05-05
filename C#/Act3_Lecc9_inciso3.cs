29127ca6ae50c9a18e930cce0931df90986e448e
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-04 20:56:37 -0600

    Add files via upload

---
+++ C#/Act3_Lecc9_inciso3.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, i = 1, divisores = 0;
+        Console.WriteLine("Ingrese un numero: ");
+        num = Convert.ToDouble(Console.ReadLine());
+        while (i <= num)
+        {
+            if (num % i == 0) { divisores = divisores + 1; }
+            i = i + 1;
+        }
+        if (divisores == 2) { Console.WriteLine("Es PRIMO"); }
+        else { Console.WriteLine("NO es primo"); }
+    }
+    }
+}
