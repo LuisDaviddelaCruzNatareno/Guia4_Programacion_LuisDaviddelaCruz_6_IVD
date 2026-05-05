2e1539437f079a1d760e01996633cf8b94b353f2
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 16:10:32 -0600

    Add files via upload

---
+++ C#/Act2_Lecc10_inciso1.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, factorial = 1, i;
+        Console.Write("Ingrese un numero para factorial: ");
+        num = Convert.ToDouble(Console.ReadLine());
+        i = num;
+
+        do
+        {
+            factorial = factorial * i;
+            i = i - 1;
+        } while (i > 0);
+
+        Console.WriteLine("El factorial es: " + factorial);
+    }
+}
