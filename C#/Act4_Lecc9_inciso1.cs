b1f69aa654b888029407bdbf856cbbcc82544582
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-04 21:00:46 -0600

    Add files via upload

---
+++ C#/Act4_Lecc9_inciso1.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, i = 1;
+        Console.WriteLine("Ingrese tabla a mostrar: ");
+        num = Convert.ToDouble(Console.ReadLine());
+        while (i <= 10)
+        {
+            Console.WriteLine(num + " x " + i + " = " + (num * i));
+            i = i + 1;
+        }
+        Console.WriteLine("Fin de la tabla.");
+    }
+}
