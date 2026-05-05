bc01612ce55297272839d7aa1ed33cec6d347350
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-04 20:10:42 -0600

    Add files via upload

--- C#/Act2_Lecc9_inciso1.cs
+++ C#/Act2_Lecc9_inciso1.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double monedas;
+        double total = 0;
+
+        Console.WriteLine("**** NIVEL 5 BLOQUEADO ****");
+
+        while (total < 350)
+        {
+            Console.WriteLine("Monedas actuales: " + total);
+            Console.Write("Ingrese monedas ganadas: ");
+            monedas = Convert.ToDouble(Console.ReadLine());
+
+            total = total + monedas;
+        }
+}
