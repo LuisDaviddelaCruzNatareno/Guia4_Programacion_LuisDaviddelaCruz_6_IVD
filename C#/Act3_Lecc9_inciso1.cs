19cd8a816fa963ba5a5fcd732274668c06c98584
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-04 20:37:24 -0600

    Add files via upload

---
+++ C#/Act3_Lecc9_inciso1.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, suma = 0;
+        Console.WriteLine("Ingrese un numero (0 para terminar): ");
+        num = Convert.ToDouble(Console.ReadLine());
+        while (num != 0)
+        {
+            suma = suma + num;
+            Console.WriteLine("Suma actual: " + suma);
+            Console.Write("Ingrese otro numero: ");
+            num = Convert.ToDouble(Console.ReadLine());
+        }
+        Console.WriteLine("Suma final: " + suma);
+    }
+}
