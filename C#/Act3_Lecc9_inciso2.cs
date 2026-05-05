0eaea1330c8dbda85b71f80967b40694d8a01efb
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-04 20:44:55 -0600

    Add files via upload

---
+++ C#/Act3_Lecc9_inciso2.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, contador = 20;
+        Console.WriteLine("Ingrese el numero limite: ");
+        num = Convert.ToDouble(Console.ReadLine());
+        while (contador <= num)
+        {
+            if (contador % 2 != 0) { Console.WriteLine("Impar: " + contador); }
+            contador = contador + 1;
+        }
+    }
+}
