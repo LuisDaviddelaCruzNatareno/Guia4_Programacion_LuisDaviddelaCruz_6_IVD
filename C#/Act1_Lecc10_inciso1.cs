389f2a900c03f47445dfa8351f820da168725008
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 16:02:19 -0600

    Add files via upload

---
+++ C#/Act1_Lecc10_inciso1.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double carros = 200;
+        double ingreso;
+
+        do
+        {
+            Console.WriteLine("Cajones disponibles: " + carros);
+            Console.Write("Presione 1 para ingresar auto: ");
+            ingreso = Convert.ToDouble(Console.ReadLine());
+            carros = carros - 1;
+        } while (carros > 0);
+
+        Console.WriteLine("Ya no hay cajones libres");
+    }
+}
