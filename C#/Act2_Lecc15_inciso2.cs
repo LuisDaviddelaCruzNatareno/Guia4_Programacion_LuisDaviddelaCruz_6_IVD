a7a49b3b903315ae391d986f193b5d1f2ce15326
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 21:29:14 -0600

    Add files via upload

---
+++ C#/Act2_Lecc15_inciso2.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double num, suma = 0;
+        Console.Write("Ingrese el numero limite: ");
+        num = Convert.ToDouble(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            // Verificamos si es múltiplo de 3 o de 5
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                suma = suma + i;
+                Console.WriteLine("Multiplo encontrado: " + i);
+            }
+        }
+        Console.WriteLine("La suma total de los multiplos es: " + suma);
+    }
+}
