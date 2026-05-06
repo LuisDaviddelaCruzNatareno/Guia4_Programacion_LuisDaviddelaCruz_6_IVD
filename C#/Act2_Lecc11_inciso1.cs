0faa9188d4b473b4860fae5fed95628f017ed106
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:25:33 -0600

    Add files via upload

---
+++ C#/Act2_Lecc11_inciso1.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Primeros 20 multiplos de 4:");
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine("4 x " + i + " = " + (i * 4));
+        }
+    }
+}
