b87a0fe492d0653db148b2469eff31587cbcfd0f
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:47:30 -0600

    Add files via upload

---
+++ C#/Act3_Lecc11_inciso2.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0, suma = 0;
+        Console.WriteLine("Ingrese numeros para sumar (ingrese 9 para terminar):");
+
+        while (num != 9)
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num != 9)
+            {
+                suma += num;
+            }
+        }
+        Console.WriteLine("La suma total es: " + suma);
+    }
+}
