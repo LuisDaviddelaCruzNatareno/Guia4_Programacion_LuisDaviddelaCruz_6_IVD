3eac00bdb038764abf9c71482df86d8a58229f0b
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:56:56 -0600

    Add files via upload

---
+++ C#/Act2_Lecc15_inciso1.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n, suma = 0;
+        while (suma < 100)
+        {
+            Console.WriteLine("Suma actual: " + suma);
+            Console.Write("Ingrese un numero: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            suma = suma + n;
+        }
+        Console.WriteLine("Meta alcanzada. Suma final: " + suma);
+    }
+}
