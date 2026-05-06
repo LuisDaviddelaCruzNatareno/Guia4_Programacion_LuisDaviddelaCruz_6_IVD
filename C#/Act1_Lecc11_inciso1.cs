f0f487f8889243f4fe7d6c4e678f56fd1142f569
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:21:23 -0600

    Add files via upload

---
+++ C#/Act1_Lecc11_inciso1.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        for(int corte = 1; corte <= 8; corte++)
+        {
+            Console.WriteLine("Obteniendo sello numero: " + corte);
+        }
+        Console.WriteLine("¡Felicidades! Puedes obtener tu corte gratis.");
+    }
+}
