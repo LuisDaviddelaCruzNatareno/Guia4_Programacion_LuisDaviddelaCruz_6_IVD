4cba7b47287a422806724ff45162e83dd0dc0a8e
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:52:59 -0600

    Add files via upload

---
+++ C#/Act1_Lecc15_inciso1.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, f = 1;
+        Console.Write("Ingrese numero para factorial: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            f = f * i;
+        }
+        Console.WriteLine("El factorial es: " + f);
+    }
+}
