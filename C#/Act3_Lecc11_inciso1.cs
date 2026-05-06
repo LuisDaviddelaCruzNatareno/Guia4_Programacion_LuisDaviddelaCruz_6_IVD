68a75d15477997523b790dd6caa78c0d05d7fc87
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:41:16 -0600

    Add files via upload

---
+++ C#/Act3_Lecc11_inciso1.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int suma = 0;
+        for (int i = 1; i <= 22; i++)
+        {
+            int divisores = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0) divisores++;
+            }
+            if (divisores == 2) suma += i;
+        }
+        Console.WriteLine("La suma de los numeros primos del 1 al 22 es: " + suma);
+    }
+}
