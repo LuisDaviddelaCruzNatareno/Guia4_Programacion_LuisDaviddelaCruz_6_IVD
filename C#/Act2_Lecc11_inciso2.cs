2426b955357a98c03076a2368c18dbb0dacde94e
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:35:54 -0600

    Add files via upload

---
+++ C#/Act2_Lecc11_inciso2.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+        Console.Write("Ingrese el numero limite: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= num; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("Par encontrado: " + i);
+            }
+        }
+    }
+}
