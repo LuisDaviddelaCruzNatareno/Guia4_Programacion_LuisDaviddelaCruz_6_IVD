5671e7104ec7fecdce986a481e3e60ccba39fdd3
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-05-05 19:12:36 -0600

    Add files via upload

---
+++ C#/Act3_Lecc10_inciso1.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int divisores, suma_primos = 0;
+
+        for (int num = 1; num <= 22; num++)
+        {
+            divisores = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    divisores = divisores + 1;
+                }
+            }
+
+            if (divisores == 2)
+            {
+                Console.WriteLine("Primo encontrado: " + num);
+                suma_primos = suma_primos + num;
+            }
+        }
+
+        Console.WriteLine("La suma total es: " + suma_primos);
+    }
+}
